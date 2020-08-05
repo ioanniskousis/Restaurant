2be96ca4bfaee5dc450d8db25c752689106ea1a5
Ioannis Kousis <bonobo@otenet.gr>
2020-08-05 19:49:22 +0300

    complete about us section

--- src/aboutUs.js
+++ src/aboutUs.js
@@ -1,14 +1,72 @@
 import { gel, crel } from './utils';
 
+function renderGrabs(content) {
+  const grabs = crel('div');
+  grabs.className = 'aboutItem';
+  grabs.style.backgroundImage = 'url("../resources/images/grabs.jpg")';
+
+  const caption = crel('div');
+  caption.className = 'aboutItemCaption';
+  caption.innerHTML = 'Fresh Supplies Daily';
+  grabs.appendChild(caption);
+
+  content.appendChild(grabs);
+}
+
+function renderChefs(content) {
+  const checfs = crel('div');
+  checfs.className = 'aboutItem';
+  checfs.style.backgroundImage = 'url("../resources/images/kitchen.jpg")';
+
+  const caption = crel('div');
+  caption.className = 'aboutItemCaption';
+  caption.innerHTML = 'Passiotate Chefs';
+  checfs.appendChild(caption);
+
+  const chef1 = crel('div');
+  chef1.className = 'chef chef1';
+  checfs.appendChild(chef1);
+
+  const chef2 = crel('div');
+  chef2.className = 'chef chef2';
+  checfs.appendChild(chef2);
+
+  const chef3 = crel('div');
+  chef3.className = 'chef chef3';
+  checfs.appendChild(chef3);
+
+  const chef4 = crel('div');
+  chef4.className = 'chef chef4';
+  checfs.appendChild(chef4);
+
+  content.appendChild(checfs);
+}
+
+function renderAtmosphaire(content) {
+  const atmosphaire = crel('div');
+  atmosphaire.className = 'aboutItem';
+  atmosphaire.style.backgroundImage = 'url("../resources/images/atmosphaire.jpg")';
+
+  const caption = crel('div');
+  caption.className = 'aboutItemCaption';
+  caption.innerHTML = 'Atmosphere of Sea';
+  atmosphaire.appendChild(caption);
+
+  content.appendChild(atmosphaire);
+}
+
 function renderAboutUs() {
   const content = gel('content');
+  renderGrabs(content);
+  renderChefs(content);
+  renderAtmosphaire(content);
 
-  let aboutItem;
-  for (let index = 0; index < 5; index += 1) {
-    aboutItem = crel('div');
-    aboutItem.className = 'aboutItem';
-    content.appendChild(aboutItem);
-  }
+  // let aboutItem;
+  // for (let index = 0; index < 5; index += 1) {
+  //   aboutItem = crel('div');
+  //   aboutItem.className = 'aboutItem';
+  //   content.appendChild(aboutItem);
+  // }
 }
 
 export default renderAboutUs;
--- src/menu.js
+++ src/menu.js
@@ -1,14 +1,81 @@
 import { gel, crel } from './utils';
 
+function salads() {
+  const items = [];
+  items.push({ image: '', description: '', price: 20 });
+
+  return items;
+}
+
+function renderHeader(content, text) {
+  const saladsHeader = crel('div');
+  saladsHeader.className = 'menuHeader';
+  saladsHeader.innerHTML = text;
+  content.appendChild(saladsHeader);
+}
+
+function renderItem(content, item) {
+  const menuItem = crel('div');
+  menuItem.className = 'menuItem';
+  content.appendChild(menuItem);
+}
+
 function renderMenu() {
   const content = gel('content');
 
-  let menuItem;
-  for (let index = 0; index < 40; index += 1) {
+  renderHeader(content, 'Salads and Appetisers');
+  const saladsItems = salads();
+  for (let index = 0; index < saladsItems.length; index += 1) {
+    renderItem(saladsItems[index]);
+  }
+  
+  renderHeader(content, 'Shellfish');
+
+  for (let index = 0; index < 4; index += 1) {
+    const menuItem = crel('div');
+    menuItem.className = 'menuItem';
+    content.appendChild(menuItem);
+  }
+
+  const fishHeader = crel('div');
+  fishHeader.className = 'menuHeader';
+  fishHeader.innerHTML = 'Fresh Fish';
+  content.appendChild(fishHeader);
+
+  for (let index = 0; index < 4; index += 1) {
+    const menuItem = crel('div');
+    menuItem.className = 'menuItem';
+    content.appendChild(menuItem);
+  }
+
+  const meatHeader = crel('div');
+  meatHeader.className = 'menuHeader';
+  meatHeader.innerHTML = 'Meat';
+  content.appendChild(meatHeader);
+
+  for (let index = 0; index < 4; index += 1) {
+    const menuItem = crel('div');
+    menuItem.className = 'menuItem';
+    content.appendChild(menuItem);
+  }
+
+  const wineHeader = crel('div');
+  wineHeader.className = 'menuHeader';
+  wineHeader.innerHTML = 'Wine';
+  content.appendChild(wineHeader);
+
+  for (let index = 0; index < 8; index += 1) {
     menuItem = crel('div');
     menuItem.className = 'menuItem';
     content.appendChild(menuItem);
   }
+
+  // for (let index = 0; index < 10; index += 1) {
+  //   menuItem = crel('div');
+  //   menuItem.className = 'menuItem';
+  //   content.appendChild(menuItem);
+  // }
+
 }
 
 export default renderMenu;
